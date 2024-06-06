18f5195b7848038f425798f1da0674e080cc7e5f
Kat Roll <kroll256@gmail.com>
2024-06-06 16:37:06 -0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'questions#index'
+  
   resources :questions
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
