27a8cfcd10f5578b4ac591f9688cfad61a4d5967
Kat Roll <kroll256@gmail.com>
2024-06-06 16:20:09 -0600

    Add IP to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', '127.0.0.1:3001'
+    origins 'localhost:3001', '127.0.0.1:3001', 'http://18.219.137.19/'
 
     resource '*',
       headers: :any,
