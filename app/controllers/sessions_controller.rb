a869b818f106eed4897ee731ea7c5319ad90e880
Kat Roll <kroll256@gmail.com>
2024-06-07 17:24:59 -0600

    Add log

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
 
   def create
-      user = User.find_by(username: params[:username])
-      
-      if user&.authenticate(params[:password])
-          session[:user_id] = user.id
-          render json: {session: user.id, user: user.to_json}, status: :created
-      else
-          render json: { error: "Invalid username or password", params: params }, status: :unauthorized
-      end
+    Rails.logger.info("Received params: #{params.inspect}")
+    user = User.find_by(username: params[:username])
+    
+    if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: {session: user.id, user: user.to_json}, status: :created
+    else
+        render json: { error: "Invalid username or password" }, status: :unauthorized
+    end
   end
 
   def destroy
-      session.delete :user_id
-      head :no_content
+    session.delete :user_id
+    head :no_content
   end
 end
