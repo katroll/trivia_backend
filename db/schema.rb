9bde4fc2e045d86cab66734483b7d3c97bab55a1
Kat Roll <kroll256@gmail.com>
2024-06-07 14:59:36 -0600

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_165601) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_07_205652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_165601) do
   create_table "questions", force: :cascade do |t|
     t.string "question"
     t.string "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
