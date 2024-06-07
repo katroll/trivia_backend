1a3a74febd05a1abf312ee70c6e24a60a32bd188
Kat Roll <kroll256@gmail.com>
2024-06-07 16:31:32 -0600

    Correct route names

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'questions#index'
 
   get "/me/:user_id", to: "users#show"
-  post "/signin", to: "sessions#create"
-  delete "/signout", to: "sessions#destroy"
+  post "/sign_in", to: "sessions#create"
+  delete "/sign_out", to: "sessions#destroy"
 
   resources :questions
   resources :categories
